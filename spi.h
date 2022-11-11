70dc5d48ea853c09c03b317d691a0db3f70fd13f
Pinky Mathew <pinky.mathew96@gmail.com>
2022-11-11 08:46:34 -0800

    Update spi.h

--- spi.h
+++ spi.h
@@ -22,7 +22,6 @@ typedef struct
     uint8_t cs;
 } spi_s;
 
-spi_s spi1 = {0,0,0,1};
 uint64_t test_input[] = {0x54, 0x66, 0x87, 0x33, 0x28, 0x15, 0x41, 0x90, 0x45, 0x78};
 
 void delay(int milli_seconds);
